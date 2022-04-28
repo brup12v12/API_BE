20b1f215820eed3caa2c90a62be7091d697ae614
brup <brup12.v@gmail.com>
2022-04-27 23:03:20 -0300

    arquivos da api

---
+++ Contexts/CharpterContext.cs
@@ -0,0 +1,32 @@
+﻿using Charpter.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Charpter.WebApi.Contexts
+{
+    public class CharpterContext : DbContext
+    {
+        public CharpterContext()
+        {
+        }
+
+        public CharpterContext(DbContextOptions<CharpterContext> options) : base(options)
+        {
+        }
+
+        //metodo utilizado para configurar o banco de dados
+
+        protected override void
+
+           OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //cada provedor tem sua sintaxe para especificação;
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-MM3CABC\\SQLEXPRESS; initial catalog = Chapter;Integrated Security = true");
+            }
+        }
+        //dbset representa as entidades  que serão utilizadas nas operãções de leitura, criação , atualização e deleção;
+
+        public DbSet<Livro>? Livros { get; set; }
+    }
+}
---
+++ Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using Charpter.WebApi.Contexts;
+using Charpter.WebApi.Models;
+
+namespace Charpter.WebApi.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly CharpterContext _context;
+        public LivroRepository(CharpterContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
