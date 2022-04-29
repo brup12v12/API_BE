eb166d6b439e734ac5d0f3ca78604ad514f76df5
brup <brup12.v@gmail.com>
2022-04-28 22:07:39 -0300

    Atualização 'CRUD'

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using Charpter.WebApi.Repositories;
+﻿using Charpter.WebApi.Models;
+using Charpter.WebApi.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,6 +35,89 @@ namespace Charpter.WebApi.Controllers
                 }
 
 
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (livroBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livroBuscado);
+
+            }
+            catch (Exception e)
+            {
+                {
+
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+
+                //return Ok("livro Cadastrado");
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                {
+
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        [HttpPut("{Id}")]
+
+        public IActionResult Cadastrar(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                {
+
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        [HttpDelete("{Id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro deletado com sucesso.");
+            }
+            catch (Exception e)
+            {
+                {
+
+                    throw new Exception(e.Message);
+                }
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Version = "v1", Title = "CharpterWebApi" });
+}); 
+
 builder.Services.AddScoped<CharpterContext, CharpterContext>();
 
 builder.Services.AddTransient<LivroRepository, LivroRepository>();
@@ -19,6 +24,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CharpterWebApi");
+    c.RoutePrefix = String.Empty;
+
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
